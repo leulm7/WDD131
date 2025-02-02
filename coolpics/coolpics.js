ddde9d88078e0c9ec94aee93527ab2bc3e2f66e6
leulm7 <leulme@byui.edu>
2025-02-01 21:19:11 -0700

    coolpics

---
+++ coolpics/coolpics.js
@@ -0,0 +1,7 @@
+const menuButton = document.querySelector(".menu-button");
+function toggleMenu() {
+  const menu = document.querySelector(".menu");
+  menu.classList.toggle("hide");
+}
+
+menuButton.addEventListener("click", toggleMenu);
